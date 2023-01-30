fe9fb9f1ffa2fd141b6660b00f38a373f4c04823
grecha1337 <4rmor3d@gmail.com>
2023-01-30 21:27:54 +0300

    edit ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/grecha1337/web-plus-pm2-deploy.git',
+      repo: 'https://github.com/grecha1337/web-plus-pm2-deploy',
       path: DEPLOY_PATH,
       'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'cd backend && npm i && npm run build && npm run start',
