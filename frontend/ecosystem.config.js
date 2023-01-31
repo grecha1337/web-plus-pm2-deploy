494426b4ba16a7c660deeb6691481fd864974bc6
grecha1337 <4rmor3d@gmail.com>
2023-01-31 21:56:04 +0300

    ecosystem-fronted

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'fronted-mesto',
+    script: './src/index.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/grecha1337/web-plus-pm2-deploy',
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd fronted && npm i && npm run build',
+    },
+  },
+};
