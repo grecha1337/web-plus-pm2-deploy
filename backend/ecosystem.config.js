17f74e6b1aa4a391c92fc55a7bf21c874737ad9f
grecha1337 <4rmor3d@gmail.com>
2023-02-01 00:00:33 +0300

    fix ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
