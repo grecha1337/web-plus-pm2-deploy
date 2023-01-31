8a879802bb9cda2a7865669ec7c216136a90fcd1
grecha1337 <4rmor3d@gmail.com>
2023-01-31 23:03:08 +0300

    add backend ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       repo: 'https://github.com/grecha1337/web-plus-pm2-deploy',
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
-      'post-deploy': 'cd backend && npm i && npm run build && cd ./dist && pm2 delete --name backend-mesto 2> /dev/null && pm2 start app.js --name backend-mesto ',
+      'post-deploy': 'cd backend && npm i && npm run build && cd ./dist && pm2 start app.js --name backend-mesto ',
     },
   },
 };
