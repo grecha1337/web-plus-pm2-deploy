d46438e41d9fc2abc6f381d72d46c8e0f58fd081
grecha1337 <4rmor3d@gmail.com>
2023-01-31 22:23:46 +0300

    fronted ecosystem

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/grecha1337/web-plus-pm2-deploy',
       path: DEPLOY_PATH,
-      'post-deploy': 'pwd && cd frontend && npm i && npm run build',
+      'post-deploy': 'cd frontend && npm i && npm run build',
     },
   },
 };
